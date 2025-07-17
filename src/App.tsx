d1558903731269aff2f59c69121fb076ba05b0da
Aniket Yadav <aniketyadav690@gmail.com>
2025-07-17 22:54:34 +0530

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center flex items-center justify-center min-h-screen">
+        Hey there!!!
+      </h1>
+    </>
+  );
+}
+
+export default App;
