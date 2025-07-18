09b8d9e9e1f6d03cd6bb0822e33272dc10e6e7c1
Aniket Yadav <aniketyadav690@gmail.com>
2025-07-18 11:55:38 +0530

    feat: store connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <h1 className="text-4xl font-bold text-center flex items-center justify-center min-h-screen">
         Hey there!!!
       </h1>
-    </>
+    </Provider>
   );
 }
 
